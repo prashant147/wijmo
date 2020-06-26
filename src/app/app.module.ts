6df752b971480a65f075965f31cf0d5dc32ca0eb
Gopimaruvada <gopi.maruvada@gmail.com>
2020-06-26 18:07:12 +0530

    added bootstarp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
+import bootstrap from "bootstrap";
 @NgModule({
   declarations: [
     AppComponent
